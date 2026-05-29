df8283b0a5ef504f6c0b598a9b65044dc2dd82bb
Takos2x1 <69368113+Takos2x1@users.noreply.github.com>
2026-05-29 16:01:06 -0400

    Conectado Tarjeta libro con detalle

--- Front/src/app/components/tarjeta-libro/tarjeta-libro.ts
+++ Front/src/app/components/tarjeta-libro/tarjeta-libro.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Libro } from '../../model/libro.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tarjeta-libro',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './tarjeta-libro.html',
   styleUrl: './tarjeta-libro.css',
 })
